7efb73ede285e6841bd3d6587f006fb125596abd
huy <minh@gmail.com>
2025-03-08 00:23:57 +0700

    nop bai tap

--- src/Biensoxe.java
+++ src/Biensoxe.java
@@ -4,25 +4,22 @@ public class Biensoxe {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("nhap so thu nhat: ");
-        int A = scanner.nextInt();
+        System.out.println("nhap bien so xe: ");
+        int n = scanner.nextInt();
 
-        System.out.println("nhap so thu hai: ");
-        int B = scanner.nextInt();
+        int sum = n % 10 ;
+        n = n / 10;
 
-        System.out.println("nhap so thu ba: ");
-        int C = scanner.nextInt();
+        sum = sum + n % 10 ;
+        n = n / 10 ;
 
-        System.out.println("nhap so thu tu: ");
-        int D = scanner.nextInt();
+        sum = sum + n % 10 ;
+        n = n / 10 ;
 
-        System.out.println("nhap so thu nam: ");
-        int E = scanner.nextInt();
+        sum = sum + n ;
+        int sonut = sum % 10;
 
-
-        int sum = A + B + C + D + E;
-        System.out.println("tong diem bien so: " + sum);
-
+        System.out.println("tong diem so nut la: " + sonut);
 
     }
 }
