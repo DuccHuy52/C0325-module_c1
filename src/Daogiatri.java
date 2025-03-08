17edb87266119ad8c92c17b9a0c6b55d7742da1e
huy <minh@gmail.com>
2025-03-08 15:05:47 +0700

    nop bai tap

--- src/Daogiatri.java
+++ src/Daogiatri.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Daogiatri {
+import java.util.Scanner;
+
+public class Daogiatri {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("nhap bien a: ");
+        int a = scanner.nextInt();
+
+        System.out.println("nhap bien b: ");
+        int b = scanner.nextInt();
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("sau khi dao gia tri, ket qua a la: " +a);
+        System.out.println("sau khi dao gia tri, ket qua b la: " +b);
+    }
 }
