53c22fb82701cbcf4504013d8514e88c865695a3
huy <minh@gmail.com>
2025-03-08 14:47:23 +0700

    nop bai tap

--- src/Boolean.java
+++ src/Boolean.java
@@ -1,6 +1,11 @@
 public class Boolean {
     public static void main(String[] args) {
 
+        boolean a = true && false; // fales
+        boolean b = (3 > 100) || (25 % 5 == 0); // true
+        boolean c = a == b ; // false
+        boolean d = ! a || b ; // true
+
 
     }
 }
